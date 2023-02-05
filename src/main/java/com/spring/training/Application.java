e2ca56a82ad801f73011514d4646911313646ac6
mamadou lamine ba <laminba2003@gmail.com>
2023-02-05 03:01:53 +0000

    improvements

--- src/main/java/com/spring/training/Application.java
+++ src/main/java/com/spring/training/Application.java
@@ -3,9 +3,11 @@ package com.spring.training;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
 @ImportResource("classpath:spring-config.xml")
+@EnableIntegration
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/spring/training/config/ApplicationConfig.java
+++ src/main/java/com/spring/training/config/ApplicationConfig.java
@@ -6,11 +6,9 @@ import com.spring.training.service.PrinterService;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.config.EnableIntegration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableIntegration
 public class ApplicationConfig {
 
     @Bean
